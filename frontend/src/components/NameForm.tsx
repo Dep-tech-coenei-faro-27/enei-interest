7cb76ea970d7d7c83696110fcb66450bd1232349
Techirocat <guerreirom230@gmail.com>
2026-06-16 12:52:23 +0100

    Adição da vereficação do Nome e envio da mensagem JSON

--- frontend/src/components/NameForm.tsx
+++ frontend/src/components/NameForm.tsx
@@ -1,19 +1,33 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function NameForm () {
 
-    const [name, setName] = useState<string>();
+    const [name, setName] = useState<string>('');
 
     const handleChange = (e : string) => {
         setName(e)
     }
 
+    const handleSubmit = () => {
+
+        if (name.trim() === ""){
+            alert("Nome tem de estar preenchido")
+            return
+        }
+
+        const mens = {
+            name: name.trim()
+        }
+
+        console.log(JSON.stringify(mens))
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={(e) => {e.preventDefault(); handleSubmit();}}>
                 <div>
                     <label>Name:</label>
-                    <input type="text" placeholder="name" value={name} onChange={() => handleChange}/>
+                    <input type="text" placeholder="name" value={name} onChange={(e) => handleChange(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Submeter"/>
