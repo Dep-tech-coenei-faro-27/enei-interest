ae4626da7f98c4fa47f277ea28b6ea3044898602
edagener0 <153123218+edagener0@users.noreply.github.com>
2026-06-17 15:29:14 +0100

    feat(submissions): adicionar schemas em falta na resposta da documentação.

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from sqlmodel import select, func
 from database.config import SessionDep, create_db_and_tables
 from fastapi import FastAPI, HTTPException
 from database.models import Submissions
-from utils.dto import SubmissionDTO
+from utils.dto import SubmissionDTO, SubmissionCountDTO
 from fastapi import status
 
 @asynccontextmanager
@@ -15,7 +15,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/api/submissions", status_code=status.HTTP_201_CREATED)
-def create_submission(submission_dto: SubmissionDTO, session: SessionDep):
+def create_submission(submission_dto: SubmissionDTO, session: SessionDep) -> Submissions:
     result = session.exec(select(Submissions).where(Submissions.email == submission_dto.email)).all()
 
     submission = Submissions.model_validate(obj = submission_dto)
@@ -30,7 +30,7 @@ def create_submission(submission_dto: SubmissionDTO, session: SessionDep):
     return submission
 
 @app.get("/api/submissions/quantity")
-def get_submission_quantity(session: SessionDep):
+def get_submission_quantity(session: SessionDep) -> SubmissionCountDTO:
 
     statement = select(func.count()).select_from(Submissions)
     quantity = session.exec(statement).one()
